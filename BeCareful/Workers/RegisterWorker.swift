2b2a8f8c4ba92f2fa1bfb6920eafd0e74ac9af60
Claudia Mariana Parente Ramos <cparente@ia.com.mx>
2020-08-24 12:53:08 -0500

    Add Scene Verify Code
    Add clean model in module verify code

--- BeCareful/Workers/RegisterWorker.swift
+++ BeCareful/Workers/RegisterWorker.swift
@@ -9,14 +9,37 @@
 import Foundation
 
 class RegisterWorker {
+    
+    private var networkClient: APIProtocol
 
+    init(networkClient: APIProtocol) {
+        self.networkClient = networkClient
+    }
+    
     // MARK: - Public Methods
-
     func validatePhone(phoneNumber: String) -> Bool {
         let phoneNumberRegex = "^[0-9]{10}$"
         let trimmedString = phoneNumber.trimmingCharacters(in: .whitespaces)
         let validate = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
         let isValidPhone = validate.evaluate(with: trimmedString)
         return isValidPhone
+    }
+    
+    func isValidCode(code: String) -> Bool {
+        return !code.isEmpty && code.count == 11
+    }
+    
+    func completeRegister(data: RegisterOTP, completion: @escaping (_ error: Error?) -> Void) {
+        networkClient.validateRegister(with: data) { result in
+            switch result {
+            case .success(let register):
+                AppUserDefaults.isRegister = true
+                AppUserDefaults.code = Int(data.otp) ?? 0
+                AppUserDefaults.token = register.token
+                completion(nil)
+            case .failure(let error):
+                completion(error)
+            }
+        }
     }
 }
